473b5fbd586903c6c501ec2182d82239a2bbb949
Christian Boyle <mail@christianboyle.com>
2023-01-14 15:21:09 -0600

    add setup, install react-router-dom, add nav and routes

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <ul>
+      <li>
+        <a href='/'>Home</a>
+      </li>
+      <li>
+        <a href='/profile'>Profile</a>
+      </li>
+    </ul>
+  )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
-import './index.css'
+import Home from './components/Home'
+import Profile from './components/Profile'
+import Header from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
 )
