c2288f2ac0f052da8d43132b926e5b21a2f9a867
Christian Boyle <mail@christianboyle.com>
2023-01-18 10:54:38 -0600

    use useNavigate to go to profile page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-  return <div>This is Home page</div>
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate('/profile')
+  }
+  return (
+    <div>
+      <button onClick={handleClick}>Show Profile Page</button>
+    </div>
+  )
 }
 
 export default Home
